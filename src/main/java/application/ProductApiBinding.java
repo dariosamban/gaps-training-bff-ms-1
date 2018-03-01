c4ca0e95926aa8da46e76a095876df978c86737c
Dario Samban <dario.samban@aa.com>
2018-03-01 13:11:48 -0600

    Calling real WS

--- src/main/java/application/ProductApiBinding.java
+++ src/main/java/application/ProductApiBinding.java
@@ -2,12 +2,16 @@ package application;
 
 //this is the class that binds to selected services
 import application.model.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ProductApiBinding {
+
+    @Value("${product.ws.endpoint}")
+    private String productEndpoint;
 
     public ApiResponseMessage<Product> delete(Long productID) {
         return new ApiResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR, "not yet implemented", null);
@@ -16,11 +20,7 @@ public class ProductApiBinding {
     public ApiResponseMessage<Product> get(Long productID) {
         RestTemplate restTemplate = new RestTemplate();
 
-        StringBuilder endpointBuilder = new StringBuilder("https://93fbc6fb-4202-4913-8666-fb7fed1ab335-bluemix.cloudant.com/product/")
-                .append(Long.valueOf(productID));
-
-        Product newProduct = restTemplate.getForObject(endpointBuilder.toString(), Product.class);
-
+        Product newProduct = restTemplate.getForObject(productEndpoint, Product.class, productID);
         return new ApiResponseMessage<>(HttpStatus.OK, "", newProduct);
     }
 

a39b7155ef6db36e7f3837d0d94a04a66fb2dbff
Dario Samban <dario.samban@aa.com>
2018-03-01 14:20:13 -0600

    Calling real WS

--- src/test/java/application/ProductApiBindingTest.java
+++ src/test/java/application/ProductApiBindingTest.java
@@ -2,17 +2,20 @@ package application;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = io.swagger.Swagger2SpringBoot.class)
 public class ProductApiBindingTest {
-    private ProductApiBinding productApiBinding;
 
-    @Before
-    public void setUp() throws Exception {
-        productApiBinding = new ProductApiBinding();
-    }
+    @Autowired
+    private ProductApiBinding productApiBinding;
 
     @Test
     public void getCallShouldReturnOKStatus() throws Exception {
